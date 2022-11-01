e734f8fdde166a82640b3def0e8d381574540aaf
Sevardon2 <pratamaengga83@gmail.com>
2022-11-01 21:11:22 +0700

    first commit

---
+++ Karyawan.js
@@ -0,0 +1,27 @@
+const karyawan = [
+    {id:'1', nama:'Engga Ananda Pratama', jabatan:'Direktur',
+    email:'engga.pratama@gmail.com', telp:'082766470620',
+    gaji:'10.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'2', nama:'Joko Heri Purnomo', jabatan:'Android Developer',
+    email:'jokoheri@gmail.com', telp:'089647559790',
+    gaji:'9.000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'3', nama:'Indah Royani Nur Sukmawati', jabatan:'Data Analist',
+    email:'indah.royani@gmail.com', telp:'088567005689',
+    gaji:'8.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'4', nama:'Kharisma Dharma Putra', jabatan:'Web Developer',
+    email:'kharismadharma@gmail.com', telp:'087354200788',
+    gaji:'8.000.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:'5', nama:'Evrilia Silvadra Putri', jabatan:'Content Writer',
+    email:'evrilia.putri@gmail.com', telp:'081236507112',
+    gaji:'6.000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'6', nama:'Ivan Yosa Ananda', jabatan:'Database Administrator',
+    email:'ivanyosa@gmail.com', telp:'082178005667',
+    gaji:'7.000.000', foto:'https://placeimg.com/100/100/tech'}
+];
+
+export default karyawan;
